f1ece948968674812cacf165063886a9a3cd16b5
Plentyne <by_amarouche@esi.dz>
2016-02-02 09:20:16 +0100

    -Rajout de la classe Depot

--- Solver-2E-VRP/Solver-2E-VRP/Model/Depot.cpp
+++ Solver-2E-VRP/Solver-2E-VRP/Model/Depot.cpp
@@ -1,2 +1,11 @@
 #include "Depot.h"
 
+// Constructors
+inline Depot::Depot() : Node() {
+
+}
+
+inline Depot::Depot(const double &x, const double &y)
+        : Node(x, y) {
+
+}
--- Solver-2E-VRP/Solver-2E-VRP/Model/Depot.h
+++ Solver-2E-VRP/Solver-2E-VRP/Model/Depot.h
@@ -1,19 +1,16 @@
 #ifndef DEPOT_H_
 #define DEPOT_H_
 
-class Depot {
-	double x;
-	double y;
+#include "Node.h"
+
+class Depot : public Node {
+
 public:
 	/** default constructor */ Depot();
-	/** constructor */ Depot(const double & coordX, const double & coordY);
-	/** constructor */ Depot(const Depot& d);
+
+    /** constructor */ Depot(const double &x, const double &y);
 
 	//access methods
-	/** set X coordination value */ void setX(const double & value);
-	/** get X coordination value */ double getX() const;
-	/** set Y coordination value */ void setY(const double & value);
-	/** get Y coordination value */ double getY() const;
 
 	// operators
 	Depot & operator=(const Depot & d);
@@ -23,22 +20,7 @@ public:
 };
 
 
-inline Depot::Depot() {
-
-}
-inline Depot::Depot(const double & coordX, const double & coordY)
-	  		 : x(coordX), y(coordY) {
-
-}
-inline Depot::Depot(const Depot& d) {
-	this->x = d.x;
-	this->y = d.y;
-}
-
-inline void Depot::setX(const double & value) { this->x = value; }
-inline double Depot::getX() const { return this->x; }
-inline void Depot::setY(const double & value) { this->y = value; }
-inline double Depot::getY() const { return this->y; }
+// Operators
 inline Depot & Depot::operator=(const Depot & d) {
 	if (this!=&d) {
 		this->x = d.x;
